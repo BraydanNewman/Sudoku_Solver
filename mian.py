007e75351de2e98c2216307a440243667186db7e
bnewman <newmanbraydan@gmail.com>
2020-08-10 00:58:10 +1000

    Update

--- mian.py
+++ mian.py
@@ -1,4 +1,6 @@
+import random
 from tkinter import *
+import tkinter as tk
 window=Tk()
 window.title("SUDOKO")
 table = [ ]
@@ -9,7 +11,9 @@ def make_board():
     for i in range(0,9):
         cols = []
         for j in range(0,9):
-            e = Entry(window, width=5, font=60)
+            enterText = tk.StringVar()
+            e = Entry(window, width=5, font=60, textvariable=enterText)
+            enterText.set(str(random.randint(3, 9)))
             e.grid(row=i, column=j)
             cols.append(e)
         table.append(cols)
@@ -17,10 +21,6 @@ def make_board():
     solve_b.grid(row=9, column=0, columnspan=9, sticky=NSEW)
 
 def backtracking():
-    for rows in table:
-        for item  in rows:
-            item.config(state=DISABLED)
-
     def row_check(y):
         if len(number_table[y]) == len(set(number_table[y])):
             check = True
@@ -31,7 +31,7 @@ def backtracking():
     def column_check(x):
         column = []
         for i in range(9):
-            column.append(number_table[x][i])
+            column.append(number_table[i][x])
         if len(column) == len(set(column)):
             check = True
         else:
@@ -47,9 +47,20 @@ def backtracking():
                 x_multiplier = 1
             else:
                 x_multiplier = 2
+            return x_multiplier
+        x_cord = quad_check(x) * 3
+        y_cord = quad_check(y) * 3
 
+        for i in range(3):
+            for j in range(3):
+                temp = number_table[y_cord + j][x_cord + i]
+                square.append(temp)
 
-
+        if len(square) == len(set(square)):
+            check = True
+        else:
+            check = False
+        return check
 
     def extract_numbers():
         number_table.clear()
@@ -59,6 +70,27 @@ def backtracking():
                 temp.append(_item.get())
             number_table.append(temp)
 
+    for rows in table:
+        for item  in rows:
+            item.config(state=DISABLED)
 
-make_board()
-window.mainloop()
+# False if double
+    extract_numbers()
+    static_table = number_table
+
+    collision_check = False
+    while not collision_check:
+        cell_num = 0
+
+        extract_numbers()
+
+        pos_check = divmod(cell_num, 9)
+
+        if row_check(pos_check[0]) and column_check(pos_check[1]) and square_check(pos_check[1], pos_check[0]):
+            cell_num = cell_num + 1
+        else:
+            pass
+
+if __name__ == "__main__":
+    make_board()
+    window.mainloop()
--- test.py
+++ test.py
@@ -1,6 +1,3 @@
-lol = [1,2,3,4,5,6,7,8,9]
+b = divmod(0,9)
 
-if len(lol) == len(set(lol)):
-    print("True")
-else:
-    print("Nope")
+print(b)
