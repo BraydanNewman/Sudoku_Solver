b12b0bf11449aab3ed006047dc296cc407c1d1e7
Braydan Newman <newmanbraydan@gmail.com>
2020-10-21 15:03:54 +1000

    Update

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Sudoku Solver
 Solves Sudoku's
+
+Icon Creator: https://www.flaticon.com/authors/freepik
+
 [Download the executable](https://github.com/BraydanNewman/Sudoku_Solver/raw/master/SudokuSolver.exe)
--- mian.py
+++ mian.py
@@ -1,112 +1,139 @@
-from tkinter import *
+import tkinter as tk
 
-window = Tk()
-window.title("SUDOKO")
-table = []
-number_table = []
+class GUI:
+    def __init__(self, master):
+        self.table = [ ]
+        self.number_table = [ ]
+        for i in range(0, 9):
+            cols = [ ]
+            for j in range(0, 9):
+                e = tk.Entry(master, width=5, font=60)
+                e.grid(row=i, column=j)
+                cols.append(e)
+            self.table.append(cols)
+        self.text = tk.StringVar()
+        self.text.set("")
+        self.solve_b = tk.Button(master, text="Solve", command=setup, bg='black', fg='white').grid(row=9, column=0, columnspan=9, sticky=tk.NSEW)
+        self.clear_b = tk.Button(master, text="Clear", command=self.clear, bg='black', fg='white').grid(row=10, column=0, columnspan=9, sticky=tk.NSEW)
+        self.label = tk.Label(master, textvariable=self.text).grid(row=11, column=0, columnspan=9, sticky=tk.NSEW)
 
-
-def make_board():
-    for i in range(0, 9):
-        cols = []
-        for j in range(0, 9):
-            e = Entry(window, width=5, font=60)
-            e.grid(row=i, column=j)
-            cols.append(e)
-        table.append(cols)
-    solve_b = Button(window, text="Solve", command=backtracking)
-    solve_b.grid(row=9, column=0, columnspan=9, sticky=NSEW)
-    clear_b = Button(window, text="Clear", command=clear)
-    clear_b.grid(row=10, column=0, columnspan=9, sticky=NSEW)
-
-
-def backtracking():
-    def full_check(y, x):
-        if row_check(y) and column_check(x) and square_check(x, y):
-            return True
-        else:
-            return False
-
-    def row_check(y):
-        temp_list = [j for j in number_table[y] if j != '']
-        if len(temp_list) == len(set(temp_list)):
-            check = True
-        else:
-            check = False
-        return check
-
-    def column_check(x):
-        column = []
-        for j in range(9):
-            column.append(number_table[j][x])
-        temp_list = [j for j in column if j != '']
-        if len(temp_list) == len(set(temp_list)):
-            check = True
-        else:
-            check = False
-        return check
-
-    def square_check(x, y):
-        square = []
-
-        def quad_check(num):
-            if num < 3:
-                x_multiplier = 0
-            elif num < 6:
-                x_multiplier = 1
-            else:
-                x_multiplier = 2
-            return x_multiplier
-
-        x_cord = quad_check(x) * 3
-        y_cord = quad_check(y) * 3
-
-        for f in range(3):
-            for j in range(3):
-                temp = number_table[y_cord + j][x_cord + f]
-                square.append(temp)
-        temp_list = [a for a in square if a != '']
-        if len(temp_list) == len(set(temp_list)):
-            check = True
-        else:
-            check = False
-        return check
-
-    def extract_numbers():
-        number_table.clear()
-        for _rows in table:
+    def extract_numbers(self):
+        self.number_table.clear()
+        for _rows in self.table:
             temp = []
             for _item in _rows:
                 number = _item.get()
                 if number != "":
                     number = int(_item.get())
                 temp.append(number)
-            number_table.append(temp)
+            self.number_table.append(temp)
 
-    extract_numbers()
-    static_table = []
-    for rows in number_table:
-        static_table_row = []
-        for item in rows:
-            static_table_row.append(item)
-        static_table.append(static_table_row)
+    def label_text(self, text):
+        self.text.set(text)
+
+    def draw_board(self, y, x):
+        self.table[ y ][ x ].delete(0, "end")
+        self.table[ y ][ x ].insert(0, self.number_table[ y ][ x ])
+
+    def clear(self):
+        for i in range(9):
+            for j in range(9):
+                self.table[ i ][ j ].delete(0, "end")
+        self.label_text("Cleared")
+
+
+def full_check(y, x):
+    if row_check(y) and column_check(x) and square_check(x, y):
+        return True
+    else:
+        return False
+
+
+def row_check(y):
+    temp_list = [j for j in app.number_table[y] if j != '']
+    if len(temp_list) == len(set(temp_list)):
+        check = True
+    else:
+        check = False
+    return check
+
+
+def column_check(x):
+    column = []
+    for j in range(9):
+        column.append(app.number_table[j][x])
+    temp_list = [j for j in column if j != '']
+    if len(temp_list) == len(set(temp_list)):
+        check = True
+    else:
+        check = False
+    return check
+
+
+def square_check(x, y):
+    square = []
+
+    def quad_check(num):
+        if num < 3:
+            x_multiplier = 0
+        elif num < 6:
+            x_multiplier = 1
+        else:
+            x_multiplier = 2
+        return x_multiplier
+
+    x_cord = quad_check(x) * 3
+    y_cord = quad_check(y) * 3
+
+    for f in range(3):
+        for j in range(3):
+            temp = app.number_table[y_cord + j][x_cord + f]
+            square.append(temp)
+    temp_list = [a for a in square if a != '']
+    if len(temp_list) == len(set(temp_list)):
+        check = True
+    else:
+        check = False
+    return check
+
+
+def setup():
+    app.extract_numbers()
+    init_check = True
+    for i in range(9):
+        for j in range(9):
+            if not full_check(i, j):
+                init_check = False
+    if init_check:
+        static_table = [ ]
+        for rows in app.number_table:
+            static_table_row = [ ]
+            for item in rows:
+                static_table_row.append(item)
+            static_table.append(static_table_row)
+        backtracking(static_table)
+    else:
+        app.label_text("Unsolvable")
+
+
+def backtracking(static_table):
     main_check_bool = False
     i = 0
     direction = 0
     while not main_check_bool:
         if static_table[divmod(i, 9)[0]][divmod(i, 9)[1]] == "":
-            if number_table[divmod(i, 9)[0]][divmod(i, 9)[1]] == "":
-                number_table[divmod(i, 9)[0]][divmod(i, 9)[1]] = 1
-                draw_board(divmod(i, 9)[0], divmod(i, 9)[1])
+            if app.number_table[divmod(i, 9)[0]][divmod(i, 9)[1]] == "":
+                app.number_table[divmod(i, 9)[0]][divmod(i, 9)[1]] = 1
+                app.draw_board(divmod(i, 9)[0], divmod(i, 9)[1])
                 if full_check(divmod(i, 9)[0], divmod(i, 9)[1]):
                     direction = 1
                     i = i + 1
             else:
-                number_table[divmod(i, 9)[0]][divmod(i, 9)[1]] = number_table[divmod(i, 9)[0]][divmod(i, 9)[1]] + 1
-                draw_board(divmod(i, 9)[0], divmod(i, 9)[1])
-                if number_table[divmod(i, 9)[0]][divmod(i, 9)[1]] > 9:
-                    number_table[divmod(i, 9)[0]][divmod(i, 9)[1]] = ""
-                    draw_board(divmod(i, 9)[0], divmod(i, 9)[1])
+                app.number_table[divmod(i, 9)[0]][divmod(i, 9)[1]] = app.number_table[divmod(i, 9)[0]][divmod(i, 9)[1]] + 1
+                app.draw_board(divmod(i, 9)[0], divmod(i, 9)[1])
+                if app.number_table[divmod(i, 9)[0]][divmod(i, 9)[1]] > 9:
+                    app.number_table[divmod(i, 9)[0]][divmod(i, 9)[1]] = ""
+                    app.draw_board(divmod(i, 9)[0], divmod(i, 9)[1])
                     direction = -1
                     i = i - 1
                 else:
@@ -120,19 +147,12 @@ def backtracking():
                 i = i + 1
         if i == 81:
             main_check_bool = True
-
-
-def draw_board(y, x):
-    table[y][x].delete(0, "end")
-    table[y][x].insert(0, number_table[y][x])
-
-
-def clear():
-    for i in range(9):
-        for j in range(9):
-            table[i][j].delete(0, "end")
+            app.label_text("Solved")
 
 
 if __name__ == "__main__":
-    make_board()
-    window.mainloop()
+    window = tk.Tk()
+    window.title("SUDOKO")
+    # window.iconbitmap('sudoku.ico')
+    app = GUI(window)
+    tk.mainloop()
